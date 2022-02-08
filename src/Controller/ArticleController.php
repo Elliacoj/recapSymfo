0bda14e5bb6281c389c4808397526bcedcb82e22
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2022-02-08 09:29:49 +0100

    Add twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Service\PlaceholderImageService;
 use Error;
 use http\Env\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/articles', name:'article_')]
 class ArticleController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController {
 
-    #[Route('/', name:'list')]
-    public function list(): Response {
-        return new Response("<h1>Liste des articles</h1>");
+    #[Route('/articles', name:'articles_list')]
+    public function list(): \Symfony\Component\HttpFoundation\Response
+    {
+        $articles = [
+            new Article(),
+            new Article(),
+            new Article(),
+        ];
+
+        return $this->render('article/list.html.twig', [
+            'articles' => $articles
+        ]);
     }
-    #[Route('/add', name: 'add')]
+    /*#[Route('/', name: 'add')]
     public function add(PlaceholderImageService $placeholderImageService): Response {
         try {
             $success = $placeholderImageService->getNewImageAndSave(350, 250, 'articlexyz-thumb.png');
@@ -27,5 +39,16 @@ class ArticleController extends \Symfony\Bundle\FrameworkBundle\Controller\Abstr
         }
 
         return new Response("<div>Erreur en ajoutant l'article</div>");
+    }*/
+
+    #[Route('/article/add', name: 'article_add')]
+    public function add(): \Symfony\Component\HttpFoundation\Response
+    {
+        if(!in_array('ROLE_AUTHOR', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('article_articles_list');
+        }
+
+        return $this->render('articles/add.html.twig');
+
     }
 }
